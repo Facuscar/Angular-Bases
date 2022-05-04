2c29f20f994430941f7a1dc53194e0e3486d8f60
Facuscar <facundo.scardillo@dracoder.com>
2022-05-04 10:16:15 -0300

    Add: delete function

---
+++ src/app/heroes/hero-list/hero-list.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-list',
+  templateUrl: './hero-list.component.html'
+})
+export class HeroListComponent {
+
+  heroes: string[] = ['Spiderman', 'IronMan', 'Goku', 'Naruto'];
+  deletedHeroes: string[] = [];
+
+  deleteHero(): void {
+    if(this.heroes.length>0){
+      this.deletedHeroes.push(this.heroes.pop()!);
+    }
+  }
+}
