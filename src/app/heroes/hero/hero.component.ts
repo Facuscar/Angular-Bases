896c846d253dce0408fd3fa2b4909cbec3ab88fc
Facuscar <facundo.scardillo@dracoder.com>
2022-05-04 09:14:27 -0300

    Add: heroes list

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'app-hero',
+    templateUrl: 'hero.component.html'
+})
+export class HeroComponent{
+    name: string = 'Ironman';
+    age: number = 45;
+}
